9f68057c4293ca4c6e0737030d407686f846b416
gajop <gajo01@gmail.com>
2014-05-15 08:43:25 +0200

    working version

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
   name='Scenario Editor BA',
   description='Scenario Editor for Balanced Annihilation',
   shortname='SE_BA',
-  version='0.2',
+  version='0.2+',
   mutator='Official',
   game='Scenario Editor BA',
   shortGame='SE_BA',
   modtype=1,
-  depend = {
-	'Scenario Editor Core 0.2',
-	'Balanced Annihilation V7.78'
+  depend = {	
+    'Scenario Editor Core 0.2+',
+	'Balanced Annihilation V7.99'
   },
 }

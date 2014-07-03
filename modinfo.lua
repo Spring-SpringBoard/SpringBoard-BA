6b319988b3a13713b42e890685a29c4b7c76fe1a
Gajo Petrovic <gajop@uns.ac.rs>
2014-07-03 20:13:45 +0200

    updated to 0.3

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
   name='Scenario Editor BA',
   description='Scenario Editor for Balanced Annihilation',
   shortname='SE_BA',
-  version='0.2+',
+  version='0.3',
   mutator='Official',
   game='Scenario Editor BA',
   shortGame='SE_BA',
   modtype=1,
   depend = {
-    'Balanced Annihilation test-2491',
-	'Scenario Editor Core 0.2+',
+    'Balanced Annihilation V8.00',
+	'Scenario Editor Core 0.3',
   },
 }

9085d713914af109f1bb3ded17991788a57be0b2
gajop <gajo01@gmail.com>
2014-07-05 18:49:19 +0200

    switch version to 'git'

--- modinfo.lua
+++ modinfo.lua
@@ -2,13 +2,13 @@ return {
   name='Scenario Editor BA',
   description='Scenario Editor for Balanced Annihilation',
   shortname='SE_BA',
-  version='0.3',
+  version='git',
   mutator='Official',
   game='Scenario Editor BA',
   shortGame='SE_BA',
   modtype=1,
   depend = {
     'Balanced Annihilation V8.00',
-	'Scenario Editor Core 0.3',
+	  'Scenario Editor Core git',
   },
 }

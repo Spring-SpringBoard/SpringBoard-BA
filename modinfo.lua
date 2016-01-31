e5538e25d38bfc01b39ecab08b7e1607e57be7e2
Gajo Petrovic <gajo01@gmail.com>
2016-01-31 22:13:14 +0900

    updated modinfo.lua (Scened + rapid dependency)

--- modinfo.lua
+++ modinfo.lua
@@ -1,14 +1,14 @@
 return {
-  name='Scenario Editor BA',
-  description='Scenario Editor for Balanced Annihilation',
+  name='Scened BA',
+  description='Scened for Balanced Annihilation',
   shortname='SE_BA',
   version='git',
   mutator='Official',
-  game='Scenario Editor BA',
+  game='Scened BA',
   shortGame='SE_BA',
   modtype=1,
   depend = {
-    'Balanced Annihilation V8.00',
-	  'Scenario Editor Core git',
+    'rapid://ba:stable',
+	'Scened Core git'
   },
 }

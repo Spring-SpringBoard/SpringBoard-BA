ef6159f223b71d5ed61d860adef12b29f7f64e4a
Gajo Petrovic <gajo01@gmail.com>
2017-02-21 23:25:58 +0900

    add rapid compatible version

--- modinfo.lua
+++ modinfo.lua
@@ -2,12 +2,12 @@ return {
   name='SpringBoard BA',
   description='SpringBoard for Balanced Annihilation',
   shortname='SB_BA',
-  version='git',
+  version='$VERSION',
   mutator='Official',
   modtype=1,
   depend = {
 --	'Balanced Annihilation V9.54',
 	'rapid://ba:stable',
-	'SpringBoard Core git'
+	'rapid://sb:stable'
   },
 }

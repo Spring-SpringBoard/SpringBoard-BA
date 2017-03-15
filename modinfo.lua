1c0b97d50610ed541720361cbe57342775bcc79e
Gajo Petrovic <gajo01@gmail.com>
2017-03-15 09:47:06 +0900

    depend on test version of SBC: https://github.com/Spring-SpringBoard/SpringBoard-Core/issues/108

--- modinfo.lua
+++ modinfo.lua
@@ -1,12 +1,15 @@
 return {
-  name='SpringBoard BA',
-  description='SpringBoard for Balanced Annihilation',
-  shortname='SB_BA',
-  version='$VERSION',
-  mutator='Official',
-  modtype=1,
+  name = 'SpringBoard BA',
+  shortname = 'SB_BA',
+  game = 'SpringBoard BA',
+  shortGame = 'SB_BA',
+  description = 'SpringBoard for Balanced Annihilation',
+  version = '$VERSION',
+  mutator = 'Official',
+  modtype = 1,
   depend = {
 	'rapid://ba:stable',
-	'rapid://sbc:stable'
+    'rapid://sbc:test',
+    --'SpringBoard Core $VERSION',
   },
 }
